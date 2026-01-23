b8edfe57e6ffab97f5fb56073f86d48b67a9be06
Caio Henrique <caiohenriqueviana01@gmail.com>
2026-01-23 11:40:33 -0300

    adicionando estoque-movimentacoes/route

--- lib/db.ts
+++ lib/db.ts
@@ -5,6 +5,15 @@ import { Pool } from 'pg';
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
