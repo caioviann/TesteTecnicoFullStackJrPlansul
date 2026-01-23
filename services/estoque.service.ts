f5f5ba2fbb711f32f7dc31fdd2ae3a522fafd25e
Caio henrique <caiohenrique@MacBook-Air-de-Caio.local>
2026-01-23 09:37:33 -0300

    adicionando estoque.service

--- services/estoque.service.ts
+++ services/estoque.service.ts
@@ -4,7 +4,7 @@ import { estoque } from '@/generated/prisma/client';
 export const getAllEstoque = async (): Promise<estoque[]> => {
   return repository.findAll();
 };
-
+//
 export const getEstoqueByProdutoId = async (produto_id: bigint): Promise<estoque | null> => {
   return repository.findByProdutoId(produto_id);
 };
